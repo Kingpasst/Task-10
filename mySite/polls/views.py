f6ab2ac5c00a01d709efb5fc0f5ef053025468cc
Sihle <sihlesnkosingubane@gmail.com>
2025-02-21 14:53:06 +0200

    first commit

---
+++ mySite/polls/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question, Choice
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def index(request):
+    latest_questions = Question.objects.order_by('-pub_date')[:5] # Lastest 5 questions
+    return render(request, 'polls/index.html', {'latest_questions': latest_questions})
+
+
+@login_required
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+@login_required
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('polls:results', question_id=question.id)
+
+
+@login_required
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
